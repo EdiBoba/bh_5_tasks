340c927db5ba8ca32dd89b6da810dff7210d84ae
Dmitry Anatsko <anatskodr@gmail.com>
2021-03-01 22:33:31 +0300

    added the solution of the task "yes_no"

--- medium/yes_no.py
+++ medium/yes_no.py
@@ -2,3 +2,13 @@
 Напишите функцию yes_or_no, которая принимает список из целых чисел,
 проходит по нему и выводит Yes, если число уже встречалось и No, если нет
 """
+
+
+def yes_or_no(_list):
+    set_number = set()
+    for item in _list:
+        if item in set_number:
+            print('Yes')
+        else:
+            print('No')
+        set_number.add(item)
